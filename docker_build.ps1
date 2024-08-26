08b8f33d5041fac2dae5eaeadad8d7a4cba2d3e0
Aljaž Konečnik <aljaz.konecnik@student.um.si>
2024-08-26 17:46:14 +0200

    docker scripts

--- docker_build.ps1
+++ docker_build.ps1
@@ -1,2 +1,2 @@
-"npm run docker --prefix .\backend\" | Invoke-Expression | Out-String
-"npm run docker --prefix .\frontend\currency-conversion\" | Invoke-Expression | Out-String
+"npm run docker --prefix .\backend\" | Invoke-Expression | Write-Host
+"npm run docker --prefix .\frontend\currency-conversion\" | Invoke-Expression | Write-Host
